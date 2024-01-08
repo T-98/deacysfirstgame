ea014ffed829aa52332847d9398cfb098610db99
Divyansh Khare <dkhare@ucsc.edu>
2024-01-07 22:38:42 -0600

    Initial Character movement setup

---
+++ FlyDeacyFly/Source/FlyDeacyFly/FlyDeacyFly.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FlyDeacyFly.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlyDeacyFly, "FlyDeacyFly" );
